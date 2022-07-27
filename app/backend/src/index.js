48b52bd3ad9661880d65a1ce5b005fff151bcc0d
Fredi Wilkens <frediwilkens@gmail.com>
2022-07-27 15:29:17 -0300

    feat: user cors

--- app/backend/src/index.js
+++ app/backend/src/index.js
@@ -1,10 +1,13 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 app.use(express.json());
+
+app.use(cors());
 
 const userRoutes = require('./routes/user.routes');
 const tasksRoutes = require('./routes/tasks.routes');
