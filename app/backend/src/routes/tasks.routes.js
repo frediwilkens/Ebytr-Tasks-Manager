c96460c2d2aec0ed79121c844601d6e0c998055c
Fredi Wilkens <frediwilkens@gmail.com>
2022-07-27 14:14:23 -0300

    feat: create task middleware

--- app/backend/src/routes/tasks.routes.js
+++ app/backend/src/routes/tasks.routes.js
@@ -1,10 +1,10 @@
 const route = require('express').Router();
 const taskControllers = require('../controllers/tasks.controller');
-
 const { authMiddleware } = require('../middlewares/auth.middleware');
+const { taskValidation } = require('../middlewares/tasks.middleware');
 
 route.get('/', taskControllers.getAll);
-route.post('/', authMiddleware, taskControllers.create);
+route.post('/', authMiddleware, taskValidation, taskControllers.create);
 route.patch('/:id/finish', taskControllers.finish);
 route.delete('/:id', taskControllers.exclude);
 
